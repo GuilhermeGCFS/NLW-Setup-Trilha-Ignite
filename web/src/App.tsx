bb0e09bfc6c405c8a19ae6895e32d942eaddf36c
GuilhermeGCFS <guilhermegcfs@yahoo.com.br>
2023-01-22 03:11:56 -0300

    Aula 2 - BackEnd, FrontEnd, Mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,16 @@
 import './styles/global.css';
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
-import { Habit } from './components/Habit';
+// import { Habit } from './components/Habit';
 
-function App() {
+export function App() {
   return (
-    <div className="container p-10">
-      <div className="flex justify-between">
-        <Habit completed={1}/>
-        <Habit completed={2}/>
-        <Habit completed={3}/>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
       </div>
     </div>
   )
 }
-
-export default App
